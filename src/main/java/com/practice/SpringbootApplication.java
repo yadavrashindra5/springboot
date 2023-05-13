5c981b81c134ff54898acb93c28d431140d0c2c7
Rashindra Yadav <yadavrashindra5@gmail.com>
2023-05-14 02:04:19 +0530

    implements the commandLineRunner Interface

--- src/main/java/com/practice/SpringbootApplication.java
+++ src/main/java/com/practice/SpringbootApplication.java
@@ -1,12 +1,18 @@
 package com.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello Rashindra");
 	}
 }
